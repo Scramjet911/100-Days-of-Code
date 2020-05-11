af301f1d05a264c3aabe28425f62164d8f0833a2
Shashank Menon <massmenon@gmail.com>
2020-05-12 02:34:31 +0530

    End of Day 6

--- Day-04, 05/FontIncrease/background.js
+++ Day-04, 05/FontIncrease/background.js
@@ -1,15 +1,30 @@
-var textSizeProm = browser.storage.local.get(null);
-textSizeProm.then((textSize) =>{
-    if(JSON.stringify(Object.keys(textSize)) == "[]"){
-        textSize.fontSize = [0];
+function onError(err){
+    console.log("Error Occurred :")
+    console.log(err);
+}
+
+function changeTextSize(details){
+    if(details.frameId == 0){
+        let textSizeProm = browser.storage.local.get(null);
+        textSizeProm.then((textSize) =>{
+            if(JSON.stringify(Object.keys(textSize)) == "[]"){
+                textSize.fontSize = [0];
+            }
+            let targetSize = textSize.fontSize[0];
+            console.log("Found Font Size(background) : " + targetSize);
+
+            if(targetSize != 0){
+                let elem = document.querySelectorAll('div'), i;
+                for(i = 0; i<elem.length; i++){
+                    let style = window.getComputedStyle(elem[i],null).getPropertyValue('font-size');
+                    let fsize = parseFloat(style);
+                    elem[i].style.fontSize = (fsize + targetSize*2) + 'px';
+                    console.log("count");
+                }
+                console.log("Text Font-Size increased(background) by " + targetSize);
+            }
+        }, onError);
     }
-    if(textSize.fontSize[0] != 0){
-        let elem = document.querySelectorAll('div'), i;
-        for(i = 0; i<elem.length; i++){
-            let style = window.getComputedStyle(elem[i],null).getPropertyValue('font-size');
-            let fsize = parseFloat(style);
-            elem[i].style.fontSize = (fsize + textSize.fontSize[0]*2) + 'px';
-        }
-        console.log("Text Font-Size increased");
-    }
-});
+}
+
+browser.webNavigation.onCompleted.addListener(changeTextSize);
